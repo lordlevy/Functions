f5ecf9212ce38fafc6cb10594b25531061a4624d
Levis <akanjilevy@yahoo.com>
2022-10-10 10:16:28 +0200

    Add project files.

---
+++ loo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Innan funtionen");
+            MyFunction();
+            MyFunction();
+            Console.WriteLine("Efter funtionen");
+        }
+
+        public static void MyFunction()
+        {
+            Console.WriteLine("Inuti en function");
+        }
+    }
+}
